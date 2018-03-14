35fd53d02e6635fe2416a6eb08f96458e7deffcd
Zheng Cheng <cz0590@gmail.com>
2018-03-14 13:02:22 +0100

    remove exp feature

--- fr.inria.atlanmod.coqtl.coq/PersonTest.v
+++ fr.inria.atlanmod.coqtl.coq/PersonTest.v
@@ -11,45 +11,5 @@ Require Import Utils_Print.
 
 Compute (execute Class2Relational PersonModel).
 
-Definition model := (execute Class2Relational PersonModel).
 
-
-Fixpoint findRoot (l : list RelationalMetamodel_EObject) : option RelationalMetamodel_EObject := 
- match l with
- | e :: l' => match RelationalMetamodel_getEClass e with
-               | TableClass => Some e
-               | ColumnClass => findRoot l'
-              end
- | nil => None
- end.
-
-Definition TabletoString (t: Table) : string :=
-  match t with
-  | BuildTable id name => 
-      " id=" ++ (natToString id) ++ " name='"++name++"'"
-  end.
-
-Fixpoint PrintEObject (rm: RelationalModel) (e: RelationalMetamodel_EObject) (lv: nat) (isRef: bool) : string := 
-  match RelationalMetamodel_getEClass e with
-   | TableClass => match toRelationalMetamodel_EClass TableClass e with
-                    | Some t => "<Table" ++ TabletoString t ++ ">"++ "</Table>"
-                    | None => "ERROR"
-                   end
-   | ColumnClass => "ERROR"
-  end.
-
-
-
-
-Definition PrintXMI (rm : RelationalModel) : string :=
-  match (findRoot (allModelElements rm)) with
-  | None => "No Root Element Found."
-  | Some e => PrintEObject rm e 0 false
-  end.
-  
-
-
-  
-  
-Compute (PrintXMI model).
 
