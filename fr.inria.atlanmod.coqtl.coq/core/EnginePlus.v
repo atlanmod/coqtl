ce4c377a25c7ee2b38a2f7cf7b7075b02e103b1e
Zheng Cheng <cz0590@gmail.com>
2018-06-13 14:27:46 +0200

    an attempt to write additivity theorem

--- fr.inria.atlanmod.coqtl.coq/core/EnginePlus.v
+++ fr.inria.atlanmod.coqtl.coq/core/EnginePlus.v
@@ -28,7 +28,7 @@ Class TransformationEngineTypeClass
     getOutputPatternElementReferencesFun: OutputPatternElementDef -> list OutputPatternElementReferenceDef;
 
 
-    getModelElementsInModelLink : TargetModel -> TargetModelLink -> list TargetModelElement;
+    
 
     executeFun: TransformationDef -> SourceModel -> TargetModel;
     
@@ -41,13 +41,15 @@ Class TransformationEngineTypeClass
     applyRuleOnPatternFun: RuleDef -> TransformationDef -> SourceModel -> list SourceModelElement -> list TargetModelElement -> option (list TargetModelLink);
 
     (* Correctness (the transformation does not generate dangling links)  our CoqTL does not hold on this *)
+    getModelElementsInModelLink : TargetModel -> TargetModelLink -> list TargetModelElement;
+    
     tr_correctness : 
     forall (tr: TransformationDef) (sm : SourceModel) (tm: TargetModel) 
-               (sp : list SourceModelElement) (r: RuleDef) (tes: list TargetModelElement) (tl: TargetModelLink) (tpl : list TargetModelLink),
+               (sp : list SourceModelElement) (r: RuleDef) (tl: TargetModelLink) (tpl : list TargetModelLink),
           tm = executeFun tr sm -> 
           In r (getRulesFun tr) -> 
           incl sp (allSourceModelElements sm) ->
-          applyRuleOnPatternFun r tr sm sp tes = Some tpl ->
+          applyPatternFun tr sm sp = Some tpl ->
           incl tpl (allTargetModelLinks tm) ->
           In tl tpl -> 
           incl (getModelElementsInModelLink tm tl) (allTargetModelElements tm)
@@ -65,7 +67,23 @@ Class TransformationEngineTypeClass
     
     (* Additivity *)
     (*  remove binding, the links is a subset of tm created before *)
+    tr_additivity : 
+    forall (tr1: TransformationDef) (tr2: TransformationDef) (sm : SourceModel) (tm1: TargetModel) (tm2: TargetModel) 
+    (r1: RuleDef) (r2: RuleDef) (rs: list RuleDef) (sp : list SourceModelElement) (tp : list TargetModelElement) 
+    (tpl1 : list TargetModelLink) (tpl2 : list TargetModelLink),
+          (getRulesFun tr1) = r1 :: rs ->
+          (getRulesFun tr2) = r2 :: rs ->
+          tm1 = executeFun tr1 sm ->
+          tm2 = executeFun tr2 sm ->
+          instantiateRuleOnPatternFun r1 tr1 sm sp = Some tp ->
+          instantiateRuleOnPatternFun r2 tr2 sm sp = Some tp ->
+          applyRuleOnPatternFun r1 tr1 sm sp tp = Some tpl1 ->
+          applyRuleOnPatternFun r2 tr2 sm sp tp = Some tpl2 ->
+          incl tpl1 tpl2 ->
+          incl (allTargetModelElements tm1) (allTargetModelElements tm2) 
+    ;
     
+
     (* Well-formedness *)
     
 
