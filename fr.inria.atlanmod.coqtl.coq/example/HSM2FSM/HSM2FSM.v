a1d9e9e7030bfc8d551f39503d3763c5cfebd8e2
Zheng Cheng <cz0590@gmail.com>
2018-07-10 17:56:37 +0200

    UPDATE CASE STUDY

---
+++ fr.inria.atlanmod.coqtl.coq/example/HSM2FSM/HSM2FSM.v
@@ -0,0 +1,47 @@
+Require Import String.
+Require Import List.
+Require Import Multiset.
+Require Import ListSet.
+Require Import Omega.
+
+Require Import core.utils.tTop.
+Require Import core.Notations.
+Require Import core.CoqTL.
+
+Require Import example.HSM.
+Require Import example.FSM.
+
+Definition HSM2FSMConcrete :=
+  transformation HSM2FSM from HSMetamodel to FSMMetamodel
+    with m as HSMModel := [
+
+       rule Class2Table
+         from
+           element c class ClassEClass from ClassMetamodel
+             when true
+         to
+          [
+           output "tab"
+             element t class TableClass from RelationalMetamodel :=
+               BuildTable (getClassId c) (getClassName c)
+             links
+               [
+                 reference TableColumnsReference from RelationalMetamodel :=
+                   attrs <- getClassAttributes c m;
+                   cols <- resolveAll Class2Relational m "col" ColumnClass
+                      (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
+                   return BuildTableColumns t cols
+               ]
+          ];
+
+
+  ].
+
+(* Unset Printing Notations.*)
+(* Print Class2Relational. *)
+(* Compute maxArity (parseTransformation Class2Relational). *)
+
+Definition HSM2FSM := parseTransformation HSM2FSMConcrete.
+
+
+
