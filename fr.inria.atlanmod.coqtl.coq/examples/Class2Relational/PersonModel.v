ba5fe238f1149bd73cfb36d7a209893cb2e4af7f
Zheng Cheng <cz0590@gmail.com>
2018-07-28 17:52:18 +0200

    Improve test case

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/PersonModel.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/PersonModel.v
@@ -6,9 +6,15 @@ Require Import example.ClassMetamodel.
 
 Definition PersonModel : Model ClassMetamodel_EObject ClassMetamodel_ELink :=
   (Build_Model
-     ((ClassMetamodel_BuildEObject ClassEClass (BuildClass 0 "Person")) :: (ClassMetamodel_BuildEObject AttributeEClass (BuildAttribute 1 false "father")) :: nil)
+     (* elements *)
+     ((ClassMetamodel_BuildEObject ClassEClass (BuildClass 0 "Person")) :: (ClassMetamodel_BuildEObject AttributeEClass (BuildAttribute 1 false "father")) :: 
+      (ClassMetamodel_BuildEObject AttributeEClass (BuildAttribute 2 true "mere")) :: nil)
+     (* links *)
      ((ClassMetamodel_BuildELink ClassAttributesEReference (BuildClassAttributes (BuildClass 0 "Person") ((BuildAttribute 1 false "father")::nil))) ::
-      (ClassMetamodel_BuildELink AttributeTypeEReference (BuildAttributeType (BuildAttribute 1 false "father") (BuildClass 0 "Person"))) :: nil)
+      (ClassMetamodel_BuildELink AttributeTypeEReference (BuildAttributeType (BuildAttribute 1 false "father") (BuildClass 0 "Person"))) ::
+      (ClassMetamodel_BuildELink ClassAttributesEReference (BuildClassAttributes (BuildClass 0 "Person") ((BuildAttribute 2 true "mere")::nil))) ::
+      (ClassMetamodel_BuildELink AttributeTypeEReference (BuildAttributeType (BuildAttribute 2 true "mere") (BuildClass 0 "Person"))) :: 
+      nil)
   ).
 
 
