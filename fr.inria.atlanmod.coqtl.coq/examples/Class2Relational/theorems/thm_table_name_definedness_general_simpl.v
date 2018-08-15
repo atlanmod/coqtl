ae2c70eb0c7e12e017c47045c782b41b74b77ace
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2018-08-15 13:55:47 +0200

    updated proof

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/theorems/thm_table_name_definedness_general_simpl.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/theorems/thm_table_name_definedness_general_simpl.v
@@ -30,8 +30,8 @@ Theorem Table_name_definedness_by_surj :
      (forall (a1 : Attribute), In (ClassMetamodel_toEObject a1) (@allModelElements _ _ cm) -> (negb (beq_string (getAttributeName a1) "")))) (* precondition 2 *) ->
     ( forall (t1 : RelationalMetamodel_EObject),
         In t1 (@allModelElements _ _ rm) ->
-          (table1 <= [[TableClass]] t1  | SUCC (negb (beq_string (getTableName table1) "")) FAIL true) /\    (* postcondition 1*)
-          (column1 <= [[ColumnClass]] t1 | SUCC (negb (beq_string (getColumnName column1) "")) FAIL true)).   (* postcondition 2*)
+          (table1 <= [[TableEClass]] t1  | SUCC (negb (beq_string (getTableName table1) "")) FAIL true) /\    (* postcondition 1*)
+          (column1 <= [[ColumnEClass]] t1 | SUCC (negb (beq_string (getColumnName column1) "")) FAIL true)).   (* postcondition 2*)
 Proof.
   (* general bookkeeping *)
   intros cm rm H Hpre t1 Hintm.
@@ -47,50 +47,51 @@ Proof.
   rename H0 into Hexec.
   rename H into Hmatch.
   destruct Hpre as [Hpre1 Hpre2].
+  
   try destruct sp eqn:sp_ca; inversion Hmatch. (* try ... inversion Hmatch; elimin impossible case *)
   try destruct l eqn:l_ca; inversion Hmatch.   (* try ... inversion Hmatch; elimin impossible case *)
   (* post1 and post2 needs to be proved here*)
-  destruct c eqn:c_ca. 
-  destruct c0 eqn:c0_ca; simpl in Hmatch.
-     (* c0 = ClassEClass, prove post1 *)
-     *  unfold instantiatePattern in Hexec.           
-        unfold instantiateRuleOnPattern in Hexec. 
-        simpl in Hexec.
-        inversion Hexec as [Htp].
-        rewrite <- Htp in Hintp.
-        simpl in Hintp.
-        destruct Hintp.
-        ** rewrite <- H.
-            assert (In c ([ClassMetamodel_BuildEObject ClassEClass c1])). { simpl. left. symmetry. assumption. }
-            apply Hinclsp in H2.
-            rewrite c_ca in H2.
-            apply Hpre1 in H2.
-            split.
-            simpl.
-            - rewrite H2.
-              done.
-            - done.
-        ** done.
-      (* c0 = AttributeEClass, prove post2 *)  
-      * unfold instantiatePattern in Hexec.           
-        unfold instantiateRuleOnPattern in Hexec. 
-unfold matchPattern in Hexec. 
+  destruct c eqn:c_ca.
+  destruct clec_arg eqn:c0_ca.
+  (* c0 = ClassEClass, prove post1 *)
+  -  unfold instantiatePattern in Hexec.           
+     unfold instantiateRuleOnPattern in Hexec. 
+     simpl in Hexec.
+     inversion Hexec as [Htp].
+     rewrite <- Htp in Hintp.
+     simpl in Hintp.
+     destruct Hintp.
+     + rewrite <- H.
+       assert (In c ([Build_ClassMetamodel_EObject ClassEClass c0])). { simpl. left. symmetry. assumption. }
+                                                                     apply Hinclsp in H2.
+       rewrite c_ca in H2.
+       apply Hpre1 in H2.
+       split.
+       simpl.
+       * rewrite H2.
+         done.
+       * done.
+     + done.
+  (* c0 = AttributeEClass, prove post2 *)  
+  - unfold instantiatePattern in Hexec.           
+    unfold instantiateRuleOnPattern in Hexec. 
+    unfold matchPattern in Hexec. 
 
-        simpl in Hexec.
-        destruct (getAttributeDerived c1) eqn:derived_ca; simpl in Hexec. 
-        ** inversion Hexec.
-        ** rewrite derived_ca in Hexec.
-           simpl in Hexec.
-           inversion Hexec as [Htp].
-           rewrite <- Htp in Hintp.
-           simpl in Hintp.
-           destruct Hintp.
-           *** rewrite <- H.
-               assert (In c ([ClassMetamodel_BuildEObject AttributeEClass c1])). { simpl. left. symmetry. assumption. }
-               apply Hinclsp in H2.
-               rewrite c_ca in H2.
-               apply Hpre2 in H2.
-               done.
-           *** done.
+    simpl in Hexec.
+    destruct (getAttributeMultiValued c0) eqn:derived_ca; simpl in Hexec. 
+    + inversion Hexec.
+    + rewrite derived_ca in Hexec.
+      simpl in Hexec.
+      inversion Hexec as [Htp].
+      rewrite <- Htp in Hintp.
+      simpl in Hintp.
+      destruct Hintp.
+      * rewrite <- H.
+        assert (In c ([Build_ClassMetamodel_EObject AttributeEClass c0])). { simpl. left. symmetry. assumption. }
+                                                                          apply Hinclsp in H2.
+        rewrite c_ca in H2.
+        apply Hpre2 in H2.
+        done.
+      * done.
 Qed.
 
