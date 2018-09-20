486bcd66915bde7d040f9daaf23d9f8a08b296ea
Zheng Cheng <cz0590@gmail.com>
2018-09-20 16:43:09 +0200

    refactor!

--- fr.inria.atlanmod.coqtl.coq/examples/ClassGraph2Tree/ClassGraph2Tree.v
+++ fr.inria.atlanmod.coqtl.coq/examples/ClassGraph2Tree/ClassGraph2Tree.v
@@ -64,72 +64,29 @@ Definition allPathsTo (m : ClassModel) (l : nat) (o: Class) : list (list Class) 
             end
          ) (allPaths m l).
 
-Definition Rule_num_getForSectionType (ruleNo : nat) (tr: TransformationA ClassMetamodel ClassMetamodel) (sm: ClassModel) : Type :=
-    match (nth_error ((TransformationA_getTransformation tr) (fun c:ClassModel => nil) sm) ruleNo) with
-    | None => Error
-    | Some r => parseRule_ForSectionType (snd r)
-    end.
 
-Definition getRuleNum_ByForSectionType (path : list Class) : nat := 1.
 
-Definition path_transfer2 (path : list Class) (id: nat)  (tr: TransformationA ClassMetamodel ClassMetamodel) (sm: ClassModel) 
-  (fe : ForExpressionA) (sp: list ClassMetamodel_EObject) : option (Rule_num_getForSectionType (getRuleNum_ByForSectionType path) tr sm).
-Proof.
-destruct (nth_error ((TransformationA_getTransformation tr) (fun c:ClassModel => nil) sm) (getRuleNum_ByForSectionType path)) eqn: nth_r.
-- destruct (nth_error (TransformationA_getRules tr) (ForExpressionA_getRule fe)) eqn:nth_ra.
-      + remember (evalForExpressionFix (snd p) (RuleA_getInTypes r) sm sp) as ret.
-        unfold Rule_num_getForSectionType.
-        rewrite nth_r.
-        destruct ret eqn: ret_ca.
-        ++ destruct (nth_error l id) eqn: rett.
-           +++ exact (Some p0).
-           +++ exact None.
-        ++ exact None.
-      + exact None.
-- exact None.
-Defined.
-
-Definition Rule_num_getForSectionType_transfer (path : list Class) (id: nat) (fe : ForExpressionA) (o : OutputPatternElementExpressionA) (tr: TransformationA ClassMetamodel ClassMetamodel) 
-  (sm: ClassModel)  (r: Rule_num_getForSectionType (getRuleNum_ByForSectionType path) tr sm) : option (ForExpressionA_getForSectionType fe tr sm).
-  Proof.
-  destruct (beq_nat ((getRuleNum_ByForSectionType path))
-                    (ForExpressionA_getRule fe)) eqn: ca.
-  - unfold Rule_num_getForSectionType in r.
-    apply beq_nat_true in ca.
-    rewrite ca in r.
-    exact (Some r).
-  - exact None.
-  Defined.
-
-Definition path_transfer (path : list Class) (id: nat) (tr: TransformationA ClassMetamodel ClassMetamodel) (sm: ClassModel) (name: string) (sp: list ClassMetamodel_EObject)
- (o: OutputPatternElementExpressionA) (fe: ForExpressionA) : option (ForExpressionA_getForSectionType fe tr sm).
-Proof.
-destruct (beq_nat (getRuleNum_ByForSectionType path)
-                  (OutputPatternElementExpressionA_getRule o)) eqn: ca.
-- destruct (path_transfer2 path id tr sm fe sp) eqn: ca2.
-  -- destruct (Rule_num_getForSectionType_transfer path id fe o tr sm r) eqn: ca3.
-     --- exact (Some f).
-     --- exact None.
-  -- exact None.
-- exact None.
-Defined.
-
-Definition type_transfer (path : list Class) (id: nat) (tr: TransformationA ClassMetamodel ClassMetamodel) (sm: ClassModel) (name: string) (sp: list ClassMetamodel_EObject) 
+Definition path_type_transfer (id: nat) (tr: TransformationA ClassMetamodel ClassMetamodel) (sm: ClassModel) (name: string) (sp: list ClassMetamodel_EObject) 
    : option (matchPattern_getForSectionType tr sm name sp).
 Proof.
 destruct (find_OutputPatternElementA tr sm sp name) eqn: find_res.
-- pose (OutputPatternElementA_getOutputPatternElementExpression o) as ope.
+- remember (OutputPatternElementA_getOutputPatternElementExpression o) as ope.
   destruct (nth_error (TransformationA_getRules tr) (OutputPatternElementExpressionA_getRule ope)) eqn: ra.
-  -- pose (RuleA_getForExpression r) as f.
-     pose (path_transfer path id tr sm name sp ope f) as pth.
-     destruct pth eqn: pth_ca.
-     --- pose (ForExpressionA_type_transfer f ope tr sm f0).
-         unfold matchPattern_getForSectionType. rewrite find_res.
-         exact o0.
+  -- pose (RuleA_getForExpression r) as fe.
+     pose (evalForExpression fe tr sm sp) as fe_res.
+     destruct fe_res eqn: fe_res_ca.
+     --- pose (nth_error l id) as ret.
+         destruct ret eqn: ret_ca.
+         * unfold matchPattern_getForSectionType. rewrite find_res.
+           pose (ForExpressionA_type_transfer fe ope tr sm f) as fexpr.
+           rewrite Heqope in fexpr.
+           exact fexpr.
+         * exact None.
      --- exact None.
   -- exact None.
 - exact None.
 Defined.
+
 
 Fixpoint eq_Class (c1 c2: Class): bool :=
   match c1, c2 with 
@@ -171,15 +128,10 @@ Definition ClassGraph2Tree' :=
               ref AttributeTypeEReference :=
                 path <- i;
                 id <- index path (allPathsTo m 3 c);
-                path' <- type_transfer path id (parsePhase ClassGraph2Tree) m "cl" [[ c ]];
+                path' <- path_type_transfer id (parsePhase ClassGraph2Tree) m "cl" [[ c ]];
                 cls <- resolveIter ClassGraph2Tree m "cl" ClassEClass [[ c ]] path';
                 return BuildAttributeType a' cls
-            ]
-        ]
-       
-    ].
-
-(* ;
+            ];
           "cl" :
             c' class ClassEClass :=
               BuildClass newId (getClassName c)
@@ -190,7 +142,12 @@ Definition ClassGraph2Tree' :=
                 paths <- type_transfer (nextPaths m path) (parsePhase ClassGraph2Tree) m "cl" [[ c ]];
                 attrs <- (optionList2List (zipWith (resolveIter ClassGraph2Tree m "at" AttributeEClass _ _) (map (fun c:Class => [[ c ]]) cls)) paths);
                 return BuildClassAttributes c' attrs
-            ] *)
+            ]
+        ]
+       
+    ].
+
+(*  *)
 
 
 Close Scope coqtl.
