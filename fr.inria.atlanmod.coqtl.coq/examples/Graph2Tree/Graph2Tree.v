b71300c76a8d90ac7854f178418850e48fbb0914
Zheng Cheng <cz0590@gmail.com>
2018-10-04 14:49:21 +0200

    update graph metamodel

---
+++ fr.inria.atlanmod.coqtl.coq/examples/Graph2Tree/Graph2Tree.v
@@ -0,0 +1,155 @@
+Require Import String.
+Require Import List.
+Require Import Multiset.
+Require Import ListSet.
+Require Import Omega.
+Require Import DecimalNat.
+Require Import DecimalString.
+
+
+Require Import core.utils.tTop.
+Require Import core.Notations.
+Require Import core.Model.
+Require Import core.CoqTL.
+
+Require Import examples.Graph2Tree.GraphMetamodel.
+
+
+Open Scope coqtl.
+
+(* Definition step (m: ClassModel) (c: Class) : option (list Class) :=
+  attrs <- getClassAttributes c m;
+  return
+  concat
+    (map
+       (fun a => match getAttributeType a m with
+              | Some cls => [ cls ]
+              | None => nil
+              end
+       ) attrs). *)
+
+(* Definition nextPaths (m: ClassModel) (p: list Class) : list (list Class) :=
+  match p with
+  | c :: p' =>
+    match getClassAttributes c m with
+    | Some attrs =>
+      map
+        (fun a =>
+           match getAttributeType a m with
+           | Some cls => cls :: p
+           | None => nil
+           end
+        )
+        attrs
+    | None => nil
+    end
+  | nil => nil
+  end. *)
+
+(* Fixpoint allPathsFix (m: ClassModel) (l : nat) (p: list Class) :  list (list Class) :=
+  match l with
+  | S l' => p :: concat (map (allPathsFix m l') (nextPaths m p))
+  | 0 => [ p ]
+  end.
+ *)
+
+Definition rootNode (m : GraphModel) : Node :=
+  hd (GraphMetamodel_defaultInstanceOfEClass NodeEClass)
+     (GraphMetamodel_allInstances NodeEClass m).
+
+Definition allPaths (m : ClassModel) (l : nat) : list (list Class) :=
+  allPathsFix m l [ rootClass m ].
+
+Definition allPathsTo (m : ClassModel) (l : nat) (o: Class) : list (list Class) :=
+  filter (fun p =>
+            match p with
+            | h :: t => beq_Class h o
+            | nil => false
+            end
+         ) (allPaths m l).
+
+
+
+
+Fixpoint eq_Class (c1 c2: Class): bool :=
+  match c1, c2 with 
+   | BuildClass a1 a2, BuildClass b1 b2 => beq_string a1 b1 && beq_string a2 b2
+  end.
+
+
+Fixpoint eq_list (l1 l2: list Class): bool :=
+  match l1, l2 with 
+   | nil, nil => true
+   | a::l1', b::l2' => eq_Class a b && eq_list l1' l2'
+   | _, _ => false
+  end.
+
+Fixpoint index (l : list Class) (ll : list (list Class)) : option nat :=
+  match ll with
+   | nil => None
+   | a :: lll => if eq_list l a then Some 0 else 
+     match (index l lll) with
+      | None => None
+      | Some n => Some (1 + n)
+     end
+  end.
+
+
+Fixpoint resolveAllIter (tr: TransformationA ClassMetamodel ClassMetamodel) (sm:ClassModel) (name: string) 
+  (type: ClassMetamodel_EClass) (sps: list Class) (iters: list (list Class)) (forSection : list (list Class))
+   : (list (Metamodel.denoteModelClass type)).
+Proof.
+destruct sps eqn: sps_ca.
+- exact ( nil).
+- destruct iters eqn: iters_ca.
+  -- exact ( nil).
+  -- destruct (index l0 forSection) eqn: id_ca.
+     + destruct (resolveIter tr sm name type [[ c ]] n) eqn:it_ca.
+       ++ exact ( (d :: (resolveAllIter tr sm name type l l1 forSection))).
+       ++ exact (resolveAllIter tr sm name type l l1 forSection).
+     + exact (resolveAllIter tr sm name type l l1 forSection).
+Defined.
+
+(* id <- index path (allPathsTo m 3 c);  should be id <- index path (getForSection (matchPattern tr m [[c]])); 
+   The problem now is that find_OutputPatternElementA did not return any result
+ *)
+Definition ClassGraph2Tree' :=
+  transformation ClassGraph2Tree from ClassMetamodel to ClassMetamodel
+    with m as ClassModel := [
+
+      rule Class2Class
+        from
+          c class ClassEClass
+        for
+          i in (allPathsTo m 1 c)
+        to [
+          "att" :
+            a' class AttributeEClass :=
+              BuildAttribute newId false (getClassName c)
+            with [
+              ref AttributeTypeEReference :=
+                path <- i;
+                path_id <- index path (allPathsTo m 1 c); 
+                cls <- resolveIter (parsePhase ClassGraph2Tree) m "cl" ClassEClass [[ c ]] path_id;
+                return BuildAttributeType a' cls
+            ];
+          "cl" :
+            c' class ClassEClass :=
+              BuildClass newId (getClassName c)
+            with [
+              ref ClassAttributesEReference :=
+                path <- i;
+                cls <- step m c;
+                let attrs := resolveAllIter (parsePhase ClassGraph2Tree) m "att" AttributeEClass cls (nextPaths m path) (allPathsTo m 1 c) in
+                  return BuildClassAttributes c' attrs
+            ] 
+        ]
+       
+    ].
+
+
+
+
+Close Scope coqtl.
+
+Definition ClassGraph2Tree := parseTransformation ClassGraph2Tree'. *)
---
+++ fr.inria.atlanmod.coqtl.coq/examples/Graph2Tree/GraphModel.v
@@ -0,0 +1,17 @@
+Require Import List.
+
+Require Import core.Model.
+
+Require Import examples.Graph2Tree.GraphMetamodel.
+
+Definition PersonModel : Model GraphMetamodel_EObject GraphMetamodel_ELink :=
+  (Build_Model
+     (
+      (Build_GraphMetamodel_EObject NodeEClass (BuildNode "0" "A")) 
+      :: (Build_GraphMetamodel_EObject NodeEClass (BuildNode "1" "B")) 
+      :: nil)
+     (
+      (Build_GraphMetamodel_ELink NodeEdgesEReference (BuildNodeEdges (BuildNode "0" "A") ((BuildNode "1" "B")::nil))) 
+      :: (Build_GraphMetamodel_ELink NodeEdgesEReference (BuildNodeEdges (BuildNode "1" "B") ((BuildNode "0" "A")::nil) )) 
+      :: nil)
+  ).
