6cec01f5024572075e10bc439bb7d41f54c040e4
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2019-04-24 13:39:27 +0200

    removing comments

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Class2Relational.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Class2Relational.v
@@ -50,40 +50,4 @@ Definition Class2Relational :=
                             tb <- resolve tr m "tab" TableClass [ClassMetamodel_toEObject cl];
                             return BuildColumnReference c tb))])])]).
 
-(* Definition Class2Relational :=
-  (BuildTransformation ClassMetamodel RelationalMetamodel
-  [
-     (BuildRule [ClassEClass]
-        (fun (m: ClassModel) (c: Class) => true)
-        [
-        (BuildOutputPatternElement "tab" TableClass
-            (fun (m: ClassModel) (c: Class) => BuildTable (getClassId c) (getClassName c))
-            [
-                (BuildOutputPatternElementReference TableColumnReference
-                    (fun (m: ClassModel) (c: Class) (t: Table) (Class2Relational: Transformation) =>
-                        reference TableColumnsReference from RelationalMetamodel :=
-                        attrs <- getClassAttributes c m;
-                        cols <- resolveAll Class2Relational m "col" ColumnClass
-                            (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
-                        return BuildTableColumns t cols))
-            ])                               
-        ]);
-     (BuildRule [AttributeEClass]
-        (fun (m: ClassModel) (a: Attribute) => true)
-        [
-        (BuildOutputPatternElement "col" ColumnClass
-            (fun (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) (getAttributeName a))
-            [
-                (BuildOutputPatternElementReference ColumnReferenceReference
-                    (fun (m: ClassModel) (a: Attribute) (c: Column) (Class2Relational: Transformation) =>
-                        reference ColumnReferenceReference from RelationalMetamodel :=
-                            cl <- getAttributeType a m;
-                            tb <- resolve Class2Relational m "tab" TableClass [ClassMetamodel_toEObject cl];
-                            return BuildColumnReference c tb))
-            ])
-        ])
-  ]). *)
-
 (* Unset Printing Notations. *)
-(* Print Class2Relational. *)
-(* Check Class2Relational. *)
