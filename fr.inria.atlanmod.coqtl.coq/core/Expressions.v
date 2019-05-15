f9c9b19c7df0c0bd705c6b8b3fb07cd1d1e7ae02
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2019-05-15 16:55:07 +0200

    factorizing expression evaluation

---
+++ fr.inria.atlanmod.coqtl.coq/core/Expressions.v
@@ -0,0 +1,49 @@
+Require Import String.
+Require Import List.
+Require Import Multiset.
+Require Import ListSet.
+Require Import Omega.
+
+Require Import core.Metamodel.
+Require Import core.Model.
+Require Import core.Engine.
+Require Import core.utils.TopUtils.
+Require Import core.utils.CpdtTactics.
+
+Section Exp.
+
+  Variables (ModelElement ModelLink ModelClass ModelReference: Type)
+            (mm: Metamodel ModelElement ModelLink ModelClass ModelReference).
+  
+  Definition Model := Model ModelElement ModelLink.
+  
+  Fixpoint denoteFuncOfClasses (sclasses : list ModelClass) (otype: Type) :=
+    match sclasses with
+    | nil => otype
+    | cons class classes' => (denoteModelClass class) -> denoteFuncOfClasses classes' otype
+    end.
+
+  Fixpoint evalFuncOfClasses (intypes: list ModelClass) (otype: Type) (f: denoteFuncOfClasses intypes otype) (el: list ModelElement) : option otype.
+  Proof.
+    destruct intypes eqn:intypes1, el eqn:el1.
+    - exact None.
+    - exact None.
+    - exact None.
+    - destruct l eqn:intypes2, l0 eqn:el2.
+      + destruct (toModelClass m m0) eqn:tmc.
+        * exact (Some (f d)).
+        * exact None.
+      + exact None.
+      + exact None.
+      + destruct (toModelClass m m0) eqn:tmc.
+        * rewrite <- intypes2 in f.
+          exact (evalFuncOfClasses l otype (f d) l0).
+        * exact None.
+  Defined.
+  
+End Exp.
+
+Arguments denoteFuncOfClasses: default implicits.
+Arguments evalFuncOfClasses: default implicits.
+
+
