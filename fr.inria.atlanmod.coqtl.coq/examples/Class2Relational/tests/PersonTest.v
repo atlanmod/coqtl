228c5bc9b8135ae7f3415a17ca3a2d91879bf9da
Zheng Cheng <cz0590@gmail.com>
2019-11-04 10:28:32 +0100

    Revert "add example"
    This reverts commit 08e97ecb53f04734aef69e34c3e0fb669d14aa79.

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/tests/PersonTest.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/tests/PersonTest.v
@@ -29,7 +29,4 @@ Require Import examples.Class2Relational.tests.PersonModel.
      : TargetModel RelationalMetamodel_EObject RelationalMetamodel_ELink
  *)
 
-
-Compute (map (Metamodel.toModelClass ClassEClass) (Model.allModelElements PersonModel)).
-
 Compute execute Class2Relational PersonModel.
