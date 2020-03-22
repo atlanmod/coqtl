ef012781646d5df4ed3dc5663d23a4804597ed18
Zheng Cheng <cz0590@gmail.com>
2020-03-22 18:46:50 +0100

    UDPATE CASE STUDY

--- fr.inria.atlanmod.coqtl.coq/examples/Class2RelationalMV/Class2Relational.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2RelationalMV/Class2Relational.v
@@ -4,99 +4,103 @@ Require Import Multiset.
 Require Import ListSet.
 Require Import Omega.
 
-Require Import core.utils.tTop.
-Require Import core.Notations.
+Require Import core.utils.TopUtils.
 Require Import core.CoqTL.
+Require Import core.Metamodel.
 
 Require Import examples.Class2RelationalMV.ClassMetamodel.
 Require Import examples.Class2RelationalMV.RelationalMetamodel.
-Require Import examples.Class2RelationalMV.ClassMetamodelPattern.
 
-Open Scope coqtl.
 
-Definition Class2RelationalMVConcrete :=
-  transformation Class2Relational from ClassMetamodel to RelationalMetamodel
-    with m as ClassModel := [
+Definition Class2Relational :=
+  (BuildTransformation
+     ClassMetamodel RelationalMetamodel
+     [(BuildRule
+         ClassMetamodel RelationalMetamodel
+         "Class2Table"
+         [ClassEClass] (fun (m: ClassModel) (c:Class) => true)
+         unit (fun (m: ClassModel) (c:Class) => [tt])
+         [(BuildOutputPatternElement
+             ClassMetamodel RelationalMetamodel 
+             [ClassEClass] "tab" TableEClass
+             (fun _ (m: ClassModel) (c:Class) => BuildTable (getClassId c) (getClassName c))
+             [(BuildOutputPatternElementReference
+                 ClassMetamodel RelationalMetamodel
+                 [ClassEClass] TableEClass TableColumnsEReference
+                 (fun (tr: MatchedTransformation ClassMetamodel RelationalMetamodel)
+                    _ (m: ClassModel) (c:Class) (t: Table) =>
+                    attrs <- getClassAttributes c m;
+                    cols <- resolveAll tr m "col" ColumnEClass
+                            (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
+                    key <- resolve tr m "key" ColumnEClass [ClassMetamodel_toEObject c];
+                    return BuildTableColumns t (key::cols)))]);
+           (BuildOutputPatternElement
+             ClassMetamodel RelationalMetamodel 
+             [ClassEClass] "key" ColumnEClass
+             (fun _ (m: ClassModel) (c:Class) => BuildColumn (getClassId c) (getClassName c ++ "id")) (* which id unique might not hold *)
+             nil)  
+         ]);
+        (BuildRule
+           ClassMetamodel RelationalMetamodel
+           "SinglevaluedAttribute2Column"
+           [AttributeEClass] (fun (m: ClassModel) (a: Attribute) => negb (getAttributeMultiValued a))
+           unit (fun (m: ClassModel) (a: Attribute) => [tt])
+           [(BuildOutputPatternElement
+               ClassMetamodel RelationalMetamodel
+               [AttributeEClass] "col" ColumnEClass
+               (fun _ (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) (getAttributeName a))
+               [(BuildOutputPatternElementReference
+                   ClassMetamodel RelationalMetamodel
+                   [AttributeEClass] ColumnEClass ColumnReferenceEReference
+                   (fun (tr: MatchedTransformation ClassMetamodel RelationalMetamodel)
+                      _ (m: ClassModel) (a: Attribute) (c: Column) =>
+                            cl <- getAttributeType a m;
+                            tb <- resolve tr m "tab" TableEClass [ClassMetamodel_toEObject cl];
+                            return BuildColumnReference c tb))])]);
+        (BuildRule
+           ClassMetamodel RelationalMetamodel
+           "MultivaluedAttribute2Column"
+           [AttributeEClass] (fun (m: ClassModel) (a: Attribute) => (getAttributeMultiValued a))
+           unit (fun (m: ClassModel) (a: Attribute) => [tt])
+           [(BuildOutputPatternElement
+               ClassMetamodel RelationalMetamodel
+               [AttributeEClass] "col" ColumnEClass
+               (fun _ (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) (getAttributeName a))
+               [(BuildOutputPatternElementReference
+                   ClassMetamodel RelationalMetamodel
+                   [AttributeEClass] ColumnEClass ColumnReferenceEReference
+                   (fun (tr: MatchedTransformation ClassMetamodel RelationalMetamodel)
+                      _ (m: ClassModel) (a: Attribute) (c: Column) =>
+                            tb <- resolve tr m "pivot" TableEClass [ClassMetamodel_toEObject a];
+                            return BuildColumnReference c tb))]);
+            (BuildOutputPatternElement
+               ClassMetamodel RelationalMetamodel
+               [AttributeEClass] "pivot" TableEClass
+               (fun _ (m: ClassModel) (a: Attribute) => BuildTable (getAttributeId a) (getAttributeName a ++ "Pivot"))
+               [(BuildOutputPatternElementReference
+                 ClassMetamodel RelationalMetamodel
+                 [AttributeEClass] TableEClass TableColumnsEReference
+                 (fun (tr: MatchedTransformation ClassMetamodel RelationalMetamodel)
+                    _ (m: ClassModel) (a: Attribute) (t: Table) =>
+                            psrc <- resolve tr m "psrc" ColumnEClass [ClassMetamodel_toEObject a];
+                            ptrg <- resolve tr m "ptrg" ColumnEClass [ClassMetamodel_toEObject a];
+                            return BuildTableColumns t [psrc; ptrg]))]);
+            (BuildOutputPatternElement
+               ClassMetamodel RelationalMetamodel
+               [AttributeEClass] "psrc" ColumnEClass
+               (fun _ (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) "key")
+               nil);
+            (BuildOutputPatternElement
+               ClassMetamodel RelationalMetamodel
+               [AttributeEClass] "ptrg" ColumnEClass
+               (fun _ (m: ClassModel) (a: Attribute) => BuildColumn (getAttributeId a) (getAttributeName a))
+               [(BuildOutputPatternElementReference
+                   ClassMetamodel RelationalMetamodel
+                   [AttributeEClass] ColumnEClass ColumnReferenceEReference
+                   (fun (tr: MatchedTransformation ClassMetamodel RelationalMetamodel)
+                      _ (m: ClassModel) (a: Attribute) (c: Column) =>
+                            cl <- getAttributeType a m;
+                            tb <- resolve tr m "tab" TableEClass [ClassMetamodel_toEObject cl];
+                            return BuildColumnReference c tb))])])]).
 
-      rule Class2Table
-        from
-          c class ClassEClass
-        to [
-          "tab" :
-            t class TableEClass :=
-              BuildTable newId (getClassName c)
-            with [
-              ref TableColumnsEReference :=
-                attrs <- getClassAttributes c m;
-                cols <- (resolveAll Class2Relational m "col" ColumnEClass
-                  (map (fun a:Attribute => [[ a ]]) attrs));
-                key <- resolve Class2Relational m "key" ColumnEClass [[ c ]];
-                return BuildTableColumns t (key :: cols)
-            ]; 
-          "key" :
-            k class ColumnEClass :=
-              BuildColumn newId (getClassName c ++ "id")
-        ];
 
-      rule SinglevaluedAttribute2Column
-        from
-          a class AttributeEClass 
-            when negb (getAttributeMultiValued a)
-        to [
-          "col" :
-            c class ColumnEClass := 
-              BuildColumn newId (getAttributeName a)
-            with [
-              ref ColumnReferenceEReference :=
-                cl <- getAttributeType a m;
-                tb <- resolve Class2Relational m "tab" TableEClass [[ cl ]];
-                return BuildColumnReference c tb
-            ]
-        ];
-      
-      rule MultivaluedAttribute2Column
-        from
-          a class AttributeEClass 
-            when getAttributeMultiValued a
-        to [
-          "col" :
-            c class ColumnEClass := 
-              BuildColumn newId (getAttributeName a)
-            with [
-              ref ColumnReferenceEReference :=
-                tb <- resolve Class2Relational m "pivot" TableEClass [[ a ]];
-                return BuildColumnReference c tb
-            ];
-                 
-          "pivot" :
-            t class TableEClass := 
-              BuildTable newId (getAttributeName a ++ "Pivot")
-            with [
-               ref TableColumnsEReference :=
-                 psrc <- resolve Class2Relational m "psrc" ColumnEClass [[ a ]];
-                 ptrg <- resolve Class2Relational m "ptrg" ColumnEClass [[ a ]];
-                 return BuildTableColumns t [psrc; ptrg]
-            ];
-                 
-          "psrc" :
-            c class ColumnEClass := 
-               BuildColumn newId "key";
-                 
-          "ptrg" :
-            c class ColumnEClass := 
-              BuildColumn newId (getAttributeName a)
-            with [
-              ref ColumnReferenceEReference :=
-                cl <- getAttributeType a m;
-                tb <- resolve Class2Relational m "tab" TableEClass [[ cl ]];
-                return BuildColumnReference c tb
-            ]
-        ]
-].
-
-Close Scope coqtl.
-
-Unset Printing Notations.
-Print Class2RelationalMVConcrete.
-
-Definition Class2RelationalMV := parseTransformation Class2RelationalMVConcrete.
