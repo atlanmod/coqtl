ec77a5472f0e783fe586f3fa2cba4375f27b6150
jhtr <jhtr@users.noreply.github.com>
2020-05-13 20:40:18 +0200

    Add manual proofs of basic theorems on Class2Relational

---
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Basic_theorems.v
@@ -0,0 +1,49 @@
+Require Import String.
+
+Require Import core.utils.TopUtils.
+Require Import core.CoqTL_v2.
+Require Import core.Metamodel.
+Require Import core.Model.
+
+Require Import examples.Class2Relational.Class2Relational.
+Require Import examples.Class2Relational.ClassMetamodel.
+Require Import examples.Class2Relational.RelationalMetamodel.
+
+Theorem All_classes_match :
+  forall (cm : ClassModel) (c : Class),
+  exists (r : Rule ClassMetamodel RelationalMetamodel),
+    matchPattern Class2Relational cm [ClassMetamodel_toEObject c] = [r].
+Proof.
+  intros.
+  eexists _.
+  reflexivity.
+Qed.
+
+Theorem All_classes_instantiate :
+  forall (cm : ClassModel) (c: Class),
+  exists (t: Table),
+    instantiatePattern Class2Relational cm [ClassMetamodel_toEObject c] = Some [RelationalMetamodel_toEObject t].
+Proof.
+  intros.
+  eexists _.
+  reflexivity.
+Qed.
+
+Theorem Concrete_attributes_instantiate :
+  forall (cm : ClassModel) (a: Attribute),
+    getAttributeDerived a = false ->
+    exists (c: Column),
+      instantiatePattern Class2Relational cm [ClassMetamodel_toEObject a] = Some [RelationalMetamodel_toEObject c].
+Proof.
+  intros.
+  eexists _.
+  unfold instantiatePattern. unfold matchPattern. simpl.
+  rewrite H. simpl.
+  unfold instantiateRuleOnPattern. simpl.
+  rewrite H. simpl.
+  unfold instantiateIterationOnPattern. simpl.
+  rewrite H. simpl.
+  unfold instantiateElementOnPattern. simpl.
+  rewrite H. simpl.
+  reflexivity.
+Qed.
