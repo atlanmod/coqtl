463496269f33261e1a3843c0e749aade6a69d341
jhtr <jhtr@users.noreply.github.com>
2020-05-18 15:33:16 +0200

    Prove basic theorems using more of the abstract interface

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Basic_theorems.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Basic_theorems.v
@@ -27,7 +27,7 @@ Theorem All_classes_match' :
     In r (matchPattern Class2Relational cm [ClassMetamodel_toEObject c]).
 Proof.
   intros.
-  eexists _.
+  eexists.
   apply tr_matchPattern_in.
   split.
   - left. reflexivity.
@@ -49,6 +49,22 @@ Proof.
   reflexivity.
 Qed.
 
+Theorem All_classes_instantiate' :
+  forall (cm : ClassModel) (c: Class),
+  exists (t: Table) tp,
+    instantiatePattern Class2Relational cm [ClassMetamodel_toEObject c] = Some tp /\
+    In (RelationalMetamodel_toEObject t) tp.
+Proof.
+  intros.
+  eexists.
+  apply tr_instantiatePattern_in.
+  do 2 eexists.
+  repeat split.
+  - left. reflexivity.
+  - reflexivity.
+  - left. reflexivity.
+Qed.
+
 Theorem Concrete_attributes_instantiate :
   forall (cm : ClassModel) (a: Attribute),
     getAttributeDerived a = false ->
@@ -66,4 +82,33 @@ Proof.
   unfold instantiateElementOnPattern. simpl.
   rewrite H. simpl.
   reflexivity.
+Qed.
+
+Theorem Concrete_attributes_instantiate' :
+  forall (cm : ClassModel) (a: Attribute),
+    getAttributeDerived a = false ->
+    exists (c: Column) tp,
+      instantiatePattern Class2Relational cm [ClassMetamodel_toEObject a] = Some tp /\
+      In (RelationalMetamodel_toEObject c) tp.
+Proof.
+  intros.
+  eexists.
+  apply tr_instantiatePattern_in.
+  do 2 eexists.
+  repeat split.
+  - apply tr_matchPattern_in.
+    split.
+    + right. left. reflexivity.
+    + (* rewrite tr_matchRuleOnPattern. *)
+      unfold matchRuleOnPattern'. unfold matchRuleOnPattern. unfold evalGuard.
+      unfold Expressions.evalFunction. simpl. rewrite H. reflexivity.
+  - (* rewrite tr_instantiateRuleOnPattern. *)
+    unfold instantiateRuleOnPattern. unfold matchRuleOnPattern. unfold evalGuard.
+    unfold Expressions.evalFunction. simpl Expressions.evalFunctionFix. rewrite H.
+    unfold instantiateIterationOnPattern. unfold matchRuleOnPattern. unfold evalGuard.
+    unfold Expressions.evalFunction. simpl Expressions.evalFunctionFix. rewrite H.
+    unfold instantiateElementOnPattern. unfold matchRuleOnPattern. unfold evalGuard.
+    unfold Expressions.evalFunction. simpl Expressions.evalFunctionFix. rewrite H.
+    simpl. reflexivity.
+  - left. reflexivity.
 Qed.
