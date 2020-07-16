ad3e63f3506e945021669d2833ce776e30183738
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2020-07-16 16:40:31 +0200

    Fixing indenting again

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Class2RelationalConcreteB.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Class2RelationalConcreteB.v
@@ -40,19 +40,20 @@ Require Import Class2Relational.RelationalMetamodel.
 Open Scope coqtlb.
 
 Definition Class2Relational :=
-  transformation ClassMetamodel RelationalMetamodel [
+  transformation ClassMetamodel RelationalMetamodel 
+  [
     rule "Class2Table" 
     from [ClassEClass] 
     where (fun m c => return true)
     for (fun m c => return 1) 
     to [elem [ClassEClass] TableClass "tab"
         (fun i m c => return BuildTable (getClassId c) (getClassName c))
-          [link [ClassEClass] TableClass TableColumnsReference
-            (fun tls i m c t =>
-              attrs <- getClassAttributes c m;
-              cols <- resolveAll tls m "col" ColumnClass 
-                (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
-              return BuildTableColumns t cols)]]
+        [link [ClassEClass] TableClass TableColumnsReference
+          (fun tls i m c t =>
+            attrs <- getClassAttributes c m;
+            cols <- resolveAll tls m "col" ColumnClass 
+              (singletons (map (A:=Attribute) ClassMetamodel_toEObject attrs));
+            return BuildTableColumns t cols)]]
     ;
     rule "Attribute2Column" 
     from [AttributeEClass] 
@@ -60,11 +61,11 @@ Definition Class2Relational :=
     for (fun m a => return 1)
     to [elem [AttributeEClass] ColumnClass "col"
         (fun i m a => return (BuildColumn (getAttributeId a) (getAttributeName a)))
-          [link [AttributeEClass] ColumnClass ColumnReferenceReference
-            (fun tls i m a c =>
-              cl <- getAttributeType a m;
-              tb <- resolve tls m "tab" TableClass [ClassMetamodel_toEObject cl];
-              return BuildColumnReference c tb)]]
+        [link [AttributeEClass] ColumnClass ColumnReferenceReference
+          (fun tls i m a c =>
+            cl <- getAttributeType a m;
+            tb <- resolve tls m "tab" TableClass [ClassMetamodel_toEObject cl];
+            return BuildColumnReference c tb)]]
   ].
 
 Close Scope coqtlb.
