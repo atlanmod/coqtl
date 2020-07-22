4a5285e0d865ebf52ee5bb6b5903d1297a3a1262
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2020-07-22 09:55:01 +0200

    Stating Table_name_uniqueness

---
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Table_name_uniqueness.v
@@ -0,0 +1,43 @@
+(**
+ CoqTL user theorem: Table_name_uniqueness
+ Def: if all classes in the source model have unique name,
+      then the target tables generated in the target model
+      have unique name.
+ **)
+
+Require Import Coq.Logic.Eqdep_dec.
+Require Import Coq.Arith.Gt.
+Require Import Coq.Arith.EqNat.
+Require Import List.
+Require Import String.
+Require Import core.utils.Utils.
+
+(*Require Import core.Semantics.
+Require Import core.Certification.*)
+Require Import core.Semantics.
+Require Import core.Certification.
+Require Import core.Metamodel.
+Require Import core.Model.
+
+Require Import examples.Class2Relational.Class2Relational.
+Require Import examples.Class2Relational.ClassMetamodel.
+Require Import examples.Class2Relational.RelationalMetamodel.
+
+Theorem Table_name_uniqueness :
+forall (cm : ClassModel) (rm : RelationalModel), 
+(* transformation *) 
+rm = execute Class2Relational cm ->
+(* precondition *)   
+(forall (c1: Class) (c2: Class), 
+    In (ClassMetamodel_toObject c1) (allModelElements cm) -> 
+    In (ClassMetamodel_toObject c2) (allModelElements cm) -> 
+    c1 <> c2 -> 
+    getClassName c1 <> getClassName c2) ->
+(* postcondition *)  
+(forall (t1: Table) (t2: Table), 
+    In (RelationalMetamodel_toObject t1) (allModelElements rm) -> 
+    In (RelationalMetamodel_toObject t2) (allModelElements rm) -> 
+    t1 <> t2 -> 
+    getTableName t1 <> getTableName t2).
+Proof.
+Admitted.
