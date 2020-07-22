fcf0ede5d5cf275b38b586e75036bef5879be0cd
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2020-07-22 10:15:49 +0200

    Starting proof for Table_name_uniqueness

--- fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Table_name_uniqueness.v
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Table_name_uniqueness.v
@@ -26,7 +26,7 @@ Require Import examples.Class2Relational.RelationalMetamodel.
 Theorem Table_name_uniqueness :
 forall (cm : ClassModel) (rm : RelationalModel), 
 (* transformation *) 
-rm = execute Class2Relational cm ->
+    rm = execute Class2Relational cm ->
 (* precondition *)   
 (forall (c1: Class) (c2: Class), 
     In (ClassMetamodel_toObject c1) (allModelElements cm) -> 
@@ -40,4 +40,23 @@ rm = execute Class2Relational cm ->
     t1 <> t2 -> 
     getTableName t1 <> getTableName t2).
 Proof.
+    intros.
+    rewrite H in H1.
+    rewrite H in H2.
+    rewrite tr_execute_in_elements in H1.
+    rewrite tr_execute_in_elements in H2.
+    do 2 destruct H1, H2.
+    destruct x, x0.
+    - (* [] [] *) contradiction H4.
+    - (* [] [x::_] *) contradiction H4.
+    - (* [x::_] [] *) contradiction H5.
+    - (* [x::_] [y::_] *) destruct x, x0.
+        + (* [x] [y] *) do 2 destruct c, c0.
+            * (*[c] [c]*) specialize (H0 c1 c2).
+            apply allTuples_incl in H1.
+            apply allTuples_incl in H2.
+            unfold incl in H1.
+            unfold incl in H2.
+            specialize (H1 c1).
+            specialize (H2 c2).
 Admitted.
