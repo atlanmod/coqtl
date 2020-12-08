ce82321e6eb475f7e099a3902e848813648c41b6
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2020-12-08 11:44:37 +0100

    Added instantiation of typeclass

--- fr.inria.atlanmod.coqtl.coq/core/twophases/Certification_ByRuleSemantics.v
+++ fr.inria.atlanmod.coqtl.coq/core/twophases/Certification_ByRuleSemantics.v
@@ -3,12 +3,14 @@ Require Import String.
 Require Import core.utils.Utils.
 Require Import core.Metamodel.
 Require Import core.Model.
+Require Import Engine.
+
 Require Import Bool.
 Require Import core.Syntax.
 Require Import Arith.
 Require Import Semantics.
-Require Import Certification.
 Require Import ByRuleSemantics.
+Require Import Certification.
 
 Scheme Equality for list.
 
@@ -364,5 +366,103 @@ Section ByRuleSemanticsCertification.
         rewrite Certification.tr_execute_in_links.
         split. trivial. trivial.
     Qed. 
+
+    Instance ByRuleEngine :
+      TransformationEngine :=
+    {
+      SourceModelElement := SourceModelElement;
+      SourceModelClass := SourceModelClass;
+      SourceModelLink := SourceModelLink;
+      SourceModelReference := SourceModelReference;
+      TargetModelElement := TargetModelElement;
+      TargetModelClass := TargetModelClass;
+      TargetModelLink := TargetModelLink;
+      TargetModelReference := TargetModelReference;
+
+      Transformation := Transformation;
+      Rule := Rule;
+      OutputPatternElement := OutputPatternElement;
+      OutputPatternElementReference := OutputPatternElementReference;
+
+      TraceLink := TraceLink;
+
+      getRules := Transformation_getRules;
+
+      getInTypes := Rule_getInTypes;
+      getGuardExpr := Rule_getGuardExpr;
+      getOutputPattern := Rule_getOutputPatternElements;
+
+      getOutputElementReferences := OutputPatternElement_getOutputElementReferences;
+   
+      execute := execute;
+
+      matchPattern := matchPattern;
+      matchRuleOnPattern := matchRuleOnPattern;
+
+      instantiatePattern := instantiatePattern;
+      instantiateRuleOnPattern := instantiateRuleOnPattern;
+      instantiateIterationOnPattern := instantiateIterationOnPattern;
+      instantiateElementOnPattern := instantiateElementOnPattern;
+
+      applyPattern := applyPattern;
+      applyRuleOnPattern := applyRuleOnPattern;
+      applyIterationOnPattern := applyIterationOnPattern;
+      applyElementOnPattern := applyElementOnPattern;
+      applyReferenceOnPattern := applyReferenceOnPattern;
+
+      evalOutputPatternElementExpr := evalOutputPatternElementExpr;
+      evalIteratorExpr := evalIteratorExpr;
+
+      resolveAll := resolveAllIter;
+      resolve := resolveIter;
+
+      tr_execute_in_elements := tr_execute_in_elements;
+      tr_execute_in_links := tr_execute_in_links;
+
+      tr_matchPattern_in := tr_matchPattern_in;
+      tr_instantiatePattern_in := tr_instantiatePattern_in;
+      tr_instantiateRuleOnPattern_in := tr_instantiateRuleOnPattern_in;
+      tr_instantiateIterationOnPattern_in := tr_instantiateIterationOnPattern_in;
+
+      tr_applyPattern_in := tr_applyPattern_in;
+      tr_applyRuleOnPattern_in := tr_applyRuleOnPattern_in;
+      tr_applyIterationOnPattern_in := tr_applyIterationOnPattern_in;
+      tr_applyElementOnPattern_in := tr_applyElementOnPattern_in;
+
+      (*tr_matchPattern_None := tr_matchPattern_None;
+
+      tr_matchRuleOnPattern_None := tr_matchRuleOnPattern_None;
+
+      tr_instantiatePattern_non_None := tr_instantiatePattern_non_None;
+      tr_instantiatePattern_None := tr_instantiatePattern_None;
+
+      tr_instantiateRuleOnPattern_non_None := tr_instantiateRuleOnPattern_non_None;
+
+      tr_instantiateIterationOnPattern_non_None := tr_instantiateIterationOnPattern_non_None;
+
+      tr_instantiateElementOnPattern_None := tr_instantiateElementOnPattern_None;
+      tr_instantiateElementOnPattern_None_iterator := tr_instantiateElementOnPattern_None_iterator;
+
+      tr_applyPattern_non_None := tr_applyPattern_non_None;
+      tr_applyPattern_None := tr_applyPattern_None;
+
+      tr_applyRuleOnPattern_non_None := tr_applyRuleOnPattern_non_None;
+
+      tr_applyIterationOnPattern_non_None := tr_applyIterationOnPattern_non_None;
+
+      tr_applyElementOnPattern_non_None := tr_applyElementOnPattern_non_None;
+
+      tr_applyReferenceOnPattern_None := tr_applyReferenceOnPattern_None;
+      tr_applyReferenceOnPattern_None_iterator := tr_applyReferenceOnPattern_None_iterator;
+
+      tr_maxArity_in := tr_maxArity_in;
+
+      tr_instantiateElementOnPattern_Leaf := tr_instantiateElementOnPattern_Leaf;
+      tr_applyReferenceOnPattern_Leaf := tr_applyReferenceOnPattern_Leaf;
+      tr_matchRuleOnPattern_Leaf := tr_matchRuleOnPattern_Leaf;
+
+      tr_resolveAll_in := tr_resolveAllIter_in;
+      tr_resolve_Leaf := tr_resolveIter_Leaf';*)
+    }. 
 
 End ByRuleSemanticsCertification.
