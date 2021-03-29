4b51e147b419dc76b7a6a0fd8f9c2fae5a979b82
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-03-29 12:24:42 +0200

    some work on Expressions

--- fr.inria.atlanmod.coqtl.coq/core/Expression.v
+++ fr.inria.atlanmod.coqtl.coq/core/Expression.v
@@ -7,26 +7,15 @@ Require Import core.Model.
 
 Section Expressions.
 
-  (*Definition Expr (A: Type) (B: Type) : Type := A -> B.*)
-
-
   (* we then need mult instance of this type class, e.g.
      one for guard expression, one for outpattern etc.
      how we can choose which Instance's evalExpr to be executed at runtime?
    *)
   Class Expression :=
     { 
-      ExprType : forall (A B: Type), Type;
-      evalExpr : forall (A B:Type), ExprType A B -> (A -> B) ; 
-    }.
-
-  Instance baseExpression :
-    Expression := {
-      ExprType (A B: Type) := A -> B;
-      evalExpr {A B:Type} (f: A -> B) (a: A) := f a;
+      (*ExprType : forall (A B: Type), Type;*)
+      Expr2 (A: Type) (B: Type) (C: Type): Type;
+      evalExpr2 (A B C:Type) (f: Expr2 A B C) (a: A) (b: B): C; 
     }.
 
 End Expressions.
-Check evalExpr.
-Arguments ExprType {_ _}.
-Arguments evalExpr {_ _}.
