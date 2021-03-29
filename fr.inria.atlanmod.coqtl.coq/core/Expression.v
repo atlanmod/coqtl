bf6b82b3c04b3bf4f87a99ba78962f5b9ebcb479
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-03-29 11:47:56 +0200

    typeclass for Expressions

--- fr.inria.atlanmod.coqtl.coq/core/Expression.v
+++ fr.inria.atlanmod.coqtl.coq/core/Expression.v
@@ -7,21 +7,26 @@ Require Import core.Model.
 
 Section Expressions.
 
-  Context {SourceModelElement SourceModelLink SourceModelClass SourceModelReference: Type}.
-  Context {smm: Metamodel SourceModelElement SourceModelLink SourceModelClass SourceModelReference}.
-  Context {TargetModelElement TargetModelLink TargetModelClass TargetModelReference: Type}.
-  Context {tmm: Metamodel TargetModelElement TargetModelLink TargetModelClass TargetModelReference}.
-  Context (SourceModel := Model SourceModelElement SourceModelLink).
-  Context (TargetModel := Model TargetModelElement TargetModelLink).
-
-  Definition Expr (A: Type) (B: Type) : Type := A -> B.
+  (*Definition Expr (A: Type) (B: Type) : Type := A -> B.*)
 
 
   (* we then need mult instance of this type class, e.g.
      one for guard expression, one for outpattern etc.
      how we can choose which Instance's evalExpr to be executed at runtime?
    *)
-  Class Expression {A : Type} {B: Type} :=
-    { evalExpr : Expr A B -> A -> B ; }.
+  Class Expression :=
+    { 
+      ExprType : forall (A B: Type), Type;
+      evalExpr : forall (A B:Type), ExprType A B -> (A -> B) ; 
+    }.
+
+  Instance baseExpression :
+    Expression := {
+      ExprType (A B: Type) := A -> B;
+      evalExpr {A B:Type} (f: A -> B) (a: A) := f a;
+    }.
 
 End Expressions.
+Check evalExpr.
+Arguments ExprType {_ _}.
+Arguments evalExpr {_ _}.
