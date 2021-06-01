89fb39918e148cef0c1744b9951b72f07569b644
Zheng Cheng <cz0590@gmail.com>
2021-06-01 13:45:32 +0200

    new lemma of case study

---
+++ fr.inria.atlanmod.coqtl.coq/examples/Class2Relational/Theorems/Relational_resolve.v
@@ -0,0 +1,61 @@
+Require Import String.
+Require Import Coq.Logic.Eqdep_dec.
+Require Import Arith.
+Require Import Coq.Arith.Gt.
+Require Import Coq.Arith.EqNat.
+Require Import List.
+Require Import Omega.
+
+Require Import core.utils.Utils.
+Require Import core.Semantics.
+Require Import core.Certification.
+Require Import core.modeling.Metamodel.
+Require Import core.Model.
+
+Require Import examples.Class2Relational.Class2Relational.
+Require Import examples.Class2Relational.ClassMetamodel.
+Require Import examples.Class2Relational.RelationalMetamodel.
+
+
+Theorem Relational_Column_Reference_definedness:
+forall (cm : ClassModel) (rm : RelationalModel), 
+  (* transformation *) rm = execute Class2Relational cm ->
+  (* precondition *)  (forall (att : Attribute),
+    In (ClassMetamodel_toObject AttributeClass att) (allModelElements cm) ->
+        getAttributeType att cm <> None) ->
+  (* postcondition *)  (forall (col: Column),
+    In (RelationalMetamodel_toObject ColumnClass col) (allModelElements rm) ->
+      getColumnReference col rm <> None). 
+Proof.
+    intros cm rm tr pre.
+    intros. 
+    rewrite tr in H.
+    apply tr_execute_in_elements in H.
+    do 2 destruct H. 
+    destruct x eqn: sp_ca. (* Case analysis on source pattern *)
+    - (* Empty pattern *) contradiction H0.
+    - destruct l eqn: l_ca.
+      + (* Singleton *) 
+        apply allTuples_incl in H.
+        unfold incl in H.
+        specialize (H c).
+        assert (In c [c]). { left. reflexivity. }
+        specialize (H H1).  
+        rename x into sp.
+        do 2 destruct c. (* Case analysis on source element type *)
+        * (* [Class] *) simpl in H0.
+          destruct H0.
+          ** admit. (* contradiction in H0 *)
+          ** crush.
+        * (* [Attribute] *) destruct c0 eqn: attr_ca.
+          destruct b eqn: d_ca.
+          -- (* derived *) contradiction H0.
+          -- (* not derived *) simpl in H0.
+              destruct H0. 
+              ++ remember (applyPattern Class2Relational cm [ClassMetamodel_BuildObject AttributeClass c0]).
+                 admit. (* todo *)
+              ++ contradiction H0.
+      + (* Other patterns *) do 2 destruct c.
+        * destruct c0. destruct c; contradiction H0.
+        * destruct c0. destruct c; contradiction H0.
+Admitted.
