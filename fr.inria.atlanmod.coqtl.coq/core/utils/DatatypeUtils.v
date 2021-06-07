97daafcc33ac9d9d6a7338388e40a7a91634addd
Zheng Cheng <cz0590@gmail.com>
2021-06-07 12:22:47 +0200

    improve option_res_dec

--- fr.inria.atlanmod.coqtl.coq/core/utils/DatatypeUtils.v
+++ fr.inria.atlanmod.coqtl.coq/core/utils/DatatypeUtils.v
@@ -8,13 +8,17 @@ Definition isNone (A: Type) (e : option A) : bool :=
 
 Lemma option_res_dec :
    forall {A B: Type} (f: A -> option B),
-      forall a: A, f a <> None ->
+      forall a: A, f a <> None <->
       (exists (b: B),
           f a = Some b).
 Proof.
-  intros. destruct (f a) as [ fa | ].
-  - exists fa. reflexivity.
-  - congruence.
+  intros. 
+  split. 
+  - destruct (f a) as [ fa | ].
+    -- exists fa. reflexivity.
+    -- congruence.
+  - intro.
+    crush.
 Qed.
 
 Theorem None_is_not_non_None :
