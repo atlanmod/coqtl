c820220aacc285862baabf039f6e1b614e0ede22
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-09-25 13:47:13 +0200

    separate TraceLink.v

--- fr.inria.atlanmod.coqtl.coq/core/Syntax.v
+++ fr.inria.atlanmod.coqtl.coq/core/Syntax.v
@@ -3,6 +3,7 @@ Require Import String.
 Require Import core.utils.Utils.
 Require Import core.Model.
 Require Import core.TransformationConfiguration.
+Require Export core.TraceLink.
 
 (** * Syntax
 
@@ -12,37 +13,6 @@ Require Import core.TransformationConfiguration.
 Section Syntax.
 
   Context {tc: TransformationConfiguration}.
-
-  (** ** Traces 
-  
-         We introduce the concept of trace in the syntax to track relationship of a target element and 
-         the source pattern that generates it   *)
-
-  Inductive TraceLink : Type :=
-    buildTraceLink : 
-      (list SourceModelElement * nat * string)
-      -> TargetModelElement
-      -> TraceLink.
-
-  Definition TraceLink_getSourcePattern (tl: TraceLink):=
-    match tl with 
-      buildTraceLink (sp, i, n) te => sp
-    end.
-
-  Definition TraceLink_getIterator (tl: TraceLink):=
-    match tl with 
-      buildTraceLink (sp, i, n) te => i
-    end.
-
-  Definition TraceLink_getName (tl: TraceLink):=
-    match tl with 
-      buildTraceLink (sp, i, n) te => n
-    end.
-
-  Definition TraceLink_getTargetElement (tl: TraceLink):=
-    match tl with 
-      buildTraceLink (sp, i, n) te => te
-    end.
 
   (** ** Syntactic Elements
 
@@ -139,8 +109,6 @@ Section Syntax.
 End Syntax.
 
 (* begin hide *)
-Arguments TraceLink {_}.
-
 Arguments Transformation {_}.
 Arguments buildTransformation {_}.
 
