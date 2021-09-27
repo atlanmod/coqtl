8207dcf653e1951e7660a8180d743dcbd0e3be8c
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-09-27 13:12:34 +0200

    detailing confluence

--- fr.inria.atlanmod.coqtl.coq/core/Properties.v
+++ fr.inria.atlanmod.coqtl.coq/core/Properties.v
@@ -1,13 +1,15 @@
 Require Import core.Semantics.
 Require Import core.Syntax.
 Require Import core.TransformationConfiguration.
+Require Import List.
 
 Definition transf_incl {tc: TransformationConfiguration} (t1 t2: Transformation) := True.
-Definition targetmodel_incl {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
-Definition transf_equiv {tc: TransformationConfiguration} (t1 t2: Transformation) := True.
-Definition targetmodel_equiv {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
 Definition sourcemodel_incl {tc: TransformationConfiguration} (t1 t2: SourceModel) := True.
+Definition targetmodel_incl {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
+Definition targetmodel_equiv {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
 
+Definition Rule_eqdec: forall {tc: TransformationConfiguration}  (x y:Rule), {x = y} + {x <> y}.
+Admitted.
 
 Theorem universality :
 forall (tc: TransformationConfiguration) (f: SourceModel -> TargetModel),
@@ -16,7 +18,8 @@ Admitted.
 
 Theorem confluence :
 forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
-  transf_equiv t1 t2 -> targetmodel_equiv (execute t1 sm) (execute t2 sm).
+  (forall (r: Rule), count_occ Rule_eqdec (Transformation_getRules t1) r = count_occ Rule_eqdec (Transformation_getRules t2) r)
+  -> targetmodel_equiv (execute t1 sm) (execute t2 sm).
 Admitted.
 
 Theorem additivity :
@@ -25,5 +28,5 @@ forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceMode
 Admitted.
 
 Definition monotonicity (tc: TransformationConfiguration) (t: Transformation) :=
-    forall (sm1 sm2: SourceModel),
-    sourcemodel_incl sm1 sm2 -> targetmodel_incl (execute t sm1) (execute t sm2).
+  forall (sm1 sm2: SourceModel),
+  sourcemodel_incl sm1 sm2 -> targetmodel_incl (execute t sm1) (execute t sm2).
