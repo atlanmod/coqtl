582780cbfd1521fccb76c363db11c76d61921052
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-10-02 09:29:38 +0200

    adding Model_wellFormed

--- fr.inria.atlanmod.coqtl.coq/core/Model.v
+++ fr.inria.atlanmod.coqtl.coq/core/Model.v
@@ -26,3 +26,6 @@ Definition allModelLinks {ModelElement: Type} {ModelLink: Type} (m: Model ModelE
 Definition Model_beq {ME ML: Type} (ME_beq: ME -> ME -> bool) (ML_beq: ML -> ML -> bool) (m1 m2: Model ME ML) :=
   andb (list_beq ME_beq (@modelElements _ _ m1) (@modelElements _ _ m2))
   (list_beq ML_beq (@modelLinks _ _ m1) (@modelLinks _ _ m2)).
+
+Definition Model_wellFormed {ME ML: Type} (ME_beq: ME -> ME -> bool) (ML_beq: ML -> ML -> bool) (m: Model ME ML): Prop :=
+  (modelLinks <> nil) -> (modelElements <> nil).
