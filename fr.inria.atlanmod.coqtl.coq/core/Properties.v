492c21fb25ffcf722419bb36bdebac28006a1e87
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-10-06 18:26:14 +0200

    proof for confluence of elements, starting working on links

--- fr.inria.atlanmod.coqtl.coq/core/Properties.v
+++ fr.inria.atlanmod.coqtl.coq/core/Properties.v
@@ -10,11 +10,6 @@ Require Import core.utils.Utils.
 Require Import PeanoNat.
 Require Import Lia.
 
-Definition transf_incl {tc: TransformationConfiguration} (t1 t2: Transformation) := True.
-Definition sourcemodel_incl {tc: TransformationConfiguration} (t1 t2: SourceModel) := True.
-Definition targetmodel_incl {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
-Definition targetmodel_equiv {tc: TransformationConfiguration} (t1 t2: TargetModel) := True.
-
 Definition toTransformation (tc: TransformationConfiguration) (f: SourceModel -> TargetModel) := 
   (buildTransformation 0 [
     (buildRule "rule"%string 
@@ -100,13 +95,99 @@ Definition Transformation_equiv {tc: TransformationConfiguration} (t1 t2: Transf
 
 (* Set semantics: we think that the list of rules represents a set (we don't allow two rules to have the same name)*)
 Definition Transformation_equiv' {tc: TransformationConfiguration} (t1 t2: Transformation) := 
+  (Transformation_getArity t1 = Transformation_getArity t2) /\ 
   forall (r:Rule),
   In r (Transformation_getRules t1) <-> In r (Transformation_getRules t2).
 
+Definition TargetModel_equiv {tc: TransformationConfiguration} (m1 m2: TargetModel) :=
+  forall (e: TargetModelElement) (l: TargetModelLink),
+   (In e (allModelElements m1) <-> In e (allModelElements m2)) /\
+    (In l (allModelLinks m1) <-> In l (allModelLinks m2)).
+  
 Theorem confluence :
 forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
-  Transformation_equiv t1 t2 -> targetmodel_equiv (execute t1 sm) (execute t2 sm).
+  Transformation_equiv' t1 t2 -> TargetModel_equiv (execute t1 sm) (execute t2 sm).
 Proof.
+  unfold TargetModel_equiv.
+  unfold Transformation_equiv'.
+  simpl.
+  intros.
+  destruct H.
+  split.
+  - split.
+    + unfold instantiatePattern.
+      unfold matchPattern.
+      intros.
+      apply in_flat_map in H1. repeat destruct H1.
+      apply in_flat_map in H2. repeat destruct H2.
+      apply filter_In in H2. destruct H2.
+      apply in_flat_map. exists x. split.
+      * unfold allTuples.
+        unfold maxArity.
+        rewrite <- H.
+        assumption.
+      * apply in_flat_map.
+        exists x0.
+        split.
+        -- apply filter_In.
+           split.
+           apply H0. assumption.
+           assumption.
+        -- assumption.
+    +  unfold instantiatePattern.
+      unfold matchPattern.
+      intros.
+      apply in_flat_map in H1. repeat destruct H1.
+      apply in_flat_map in H2. repeat destruct H2.
+      apply filter_In in H2. destruct H2.
+      apply in_flat_map. exists x. split.
+      * unfold allTuples.
+        unfold maxArity.
+        rewrite H.
+        assumption.
+      * apply in_flat_map.
+        exists x0.
+        split.
+        -- apply filter_In.
+           split.
+           apply H0. assumption.
+           assumption.
+        -- assumption.
+  - split.
+    + unfold applyPattern.
+      unfold matchPattern.
+      intros.
+      apply in_flat_map in H1. repeat destruct H1.
+      apply in_flat_map in H2. repeat destruct H2.
+      apply filter_In in H2. destruct H2.
+      apply in_flat_map. exists x. split.
+      * unfold allTuples.
+        unfold maxArity.
+        rewrite <- H.
+        assumption.
+      * apply in_flat_map.
+        exists x0.
+        split.
+        -- apply filter_In.
+           split.
+           apply H0. assumption.
+           assumption.
+        -- unfold applyRuleOnPattern, applyIterationOnPattern in *.
+           apply in_flat_map in H3. repeat destruct H3.
+           apply in_flat_map.
+           exists x1.
+           split.
+           ++ assumption.
+           ++ apply in_flat_map in H5. repeat destruct H5.
+              apply in_flat_map.
+              exists x2.
+              split.
+              ** assumption.
+              ** unfold applyElementOnPattern in *.
+                 assert (trace t1 sm = trace t2 sm). {
+                   unfold trace, tracePattern.
+                   admit. (* requires reordering for rules *)
+                 }
 Admitted.
 
 Definition Model_incl {tc: TransformationConfiguration}  (m1 m2: TargetModel) : Prop := 
