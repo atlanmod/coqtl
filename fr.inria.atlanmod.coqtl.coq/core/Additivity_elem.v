2f62524c39f3212593d6059429f049f1ffaab19d
Zheng Cheng <cz0590@gmail.com>
2021-10-09 19:30:09 +0200

    Update Additivity_elem.v

--- fr.inria.atlanmod.coqtl.coq/core/Additivity_elem.v
+++ fr.inria.atlanmod.coqtl.coq/core/Additivity_elem.v
@@ -16,20 +16,20 @@ Require Import FunctionalExtensionality.
 (** * Additivity in OutputPatternElement context             *)
 (*************************************************************)
 
-Definition Rule_incl_elements {tc: TransformationConfiguration} (x y: Rule) : Prop :=
+Definition Rule_incl_elements' {tc: TransformationConfiguration} (x y: Rule) : Prop :=
   Rule_getName x = Rule_getName y
   /\ Rule_getGuardExpr x = Rule_getGuardExpr y  
   /\ Rule_getIteratorExpr x = Rule_getIteratorExpr y
   /\ (forall o1, In o1 (Rule_getOutputPatternElements x) -> In o1 (Rule_getOutputPatternElements y)).
 
-Definition Transformation_incl_elements'' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
+Definition Transformation_incl_elements''' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
   (Transformation_getArity t1 = Transformation_getArity t2) /\ 
   (forall (r1: Rule), In r1 (Transformation_getRules t1) -> 
-    (exists r2:Rule, In r2 (Transformation_getRules t2) /\ Rule_incl_elements r1 r2 )). 
+    (exists r2:Rule, In r2 (Transformation_getRules t2) /\ Rule_incl_elements' r1 r2 )). 
 
-Theorem additivity_elements_correct :
+Theorem additivity_elements_general :
 forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
-  (Transformation_incl_elements'' t1 t2 -> 
+  (Transformation_incl_elements''' t1 t2 -> 
     incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm))).
 Proof.
 simpl.
@@ -49,7 +49,7 @@ split.
   specialize (H4 x0 H1).
   destruct H4.
   exists x1.
-  unfold Rule_incl_elements in H4.
+  unfold Rule_incl_elements' in H4.
   destruct H4. destruct H5. destruct H6. destruct H7.
   split.
   + apply filter_In.
@@ -71,7 +71,65 @@ split.
        split; auto.
 Qed.
 
-  
+
+Definition Rule_incl_elements {tc: TransformationConfiguration} (x y: Rule) : Prop :=
+  Rule_getName x = Rule_getName y
+  /\ Rule_getGuardExpr x = Rule_getGuardExpr y  
+  /\ Rule_getIteratorExpr x = Rule_getIteratorExpr y
+  /\ subseq (Rule_getOutputPatternElements x) (Rule_getOutputPatternElements y).
+
+Inductive Transformation_incl_rules' {tc: TransformationConfiguration}  : list Rule -> list Rule -> Prop :=
+  | incl_rules_nil : Transformation_incl_rules' nil nil
+  | incl_rules_diff : forall x y xs ys, Transformation_incl_rules' xs ys 
+    -> Rule_incl_elements x y
+    -> Transformation_incl_rules' (x::xs) (y::ys).
+
+Definition Transformation_incl_elements'' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
+  (Transformation_getArity t1 = Transformation_getArity t2) /\ 
+  Transformation_incl_rules' (Transformation_getRules t1) (Transformation_getRules t2). 
+
+Lemma tr_incl_equiv:
+  forall (tc: TransformationConfiguration) t1 t2,
+    Transformation_incl_elements'' t1 t2 -> Transformation_incl_elements''' t1 t2.
+Proof.
+intros.
+destruct  H.
+unfold Transformation_incl_elements'''.
+split. 
+* auto.
+* intro.
+  induction H0.
+  **  intros.
+      contradiction.
+  **  intros.
+      simpl in H2.
+      destruct H2.
+      - exists y.
+        split.
+        -- crush.
+        -- rewrite <- H2.
+           unfold Rule_incl_elements in H1.
+           unfold Rule_incl_elements'; crush.
+           induction H6.
+           --- contradiction.
+           --- destruct H5; crush.
+           --- crush.
+      - specialize (IHTransformation_incl_rules' H2). 
+        destruct IHTransformation_incl_rules'.
+        destruct H3.
+        exists x0; crush.
+Qed.
+
+Theorem additivity_elements_correct :
+forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
+  (Transformation_incl_elements'' t1 t2 -> 
+    incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm))).
+Proof.
+intros.
+specialize (tr_incl_equiv tc t1 t2 H).
+specialize (additivity_elements_general tc t1 t2).
+auto.
+Qed.
 
 
 
