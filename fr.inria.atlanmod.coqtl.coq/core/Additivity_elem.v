cf2e368037ad2456e28031a597257d0eb1b284da
Zheng Cheng <cz0590@gmail.com>
2021-10-09 16:44:12 +0200

    Update Additivity_elem.v

--- fr.inria.atlanmod.coqtl.coq/core/Additivity_elem.v
+++ fr.inria.atlanmod.coqtl.coq/core/Additivity_elem.v
@@ -32,43 +32,43 @@ forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceMode
   (Transformation_incl_elements'' t1 t2 -> 
     incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm))).
 Proof.
-  simpl.
-  unfold incl.
-  intros.
-  apply in_flat_map in H0. repeat destruct H0. 
-  apply in_flat_map in H1. repeat destruct H1.
-  apply filter_In in H1. destruct H1.
-  destruct H.
-  apply in_flat_map. exists x.
-  split.
-  * unfold allTuples.
-    unfold maxArity.
-    rewrite <- H.
-    assumption.
-  * apply in_flat_map.
-    specialize (H4 x0 H1).
-    destruct H4.
-exists x1.
-unfold Rule_incl_elements in H4.
-destruct H4. destruct H5. destruct H6. destruct H7.
+simpl.
+unfold incl.
+intros.
+apply in_flat_map in H0. repeat destruct H0. 
+apply in_flat_map in H1. repeat destruct H1.
+apply filter_In in H1. destruct H1.
+destruct H.
+apply in_flat_map. exists x.
 split.
-+ apply filter_In.
-  split; auto.
-  unfold matchRuleOnPattern, evalGuardExpr.
-  unfold matchRuleOnPattern,evalGuardExpr in H3.
-  rewrite <- H6. auto.
-+ apply in_flat_map.
-  apply in_flat_map in H2.
-  destruct H2.
-  exists x2.
-  destruct H2.
+* unfold allTuples.
+  unfold maxArity.
+  rewrite <- H.
+  assumption.
+* apply in_flat_map.
+  specialize (H4 x0 H1).
+  destruct H4.
+  exists x1.
+  unfold Rule_incl_elements in H4.
+  destruct H4. destruct H5. destruct H6. destruct H7.
   split.
-  ++ unfold evalIteratorExpr; unfold evalIteratorExpr in H2. rewrite <- H7; auto.
-  ++ apply in_flat_map; apply in_flat_map in H9.
-     destruct H9. destruct H9.
-     specialize (H8 x3 H9).
-     exists x3.
-     split; auto.
+  + apply filter_In.
+    split; auto.
+    unfold matchRuleOnPattern, evalGuardExpr.
+    unfold matchRuleOnPattern,evalGuardExpr in H3.
+    rewrite <- H6. auto.
+  + apply in_flat_map.
+    apply in_flat_map in H2.
+    destruct H2.
+    exists x2.
+    destruct H2.
+    split.
+    ++ unfold evalIteratorExpr; unfold evalIteratorExpr in H2. rewrite <- H7; auto.
+    ++ apply in_flat_map; apply in_flat_map in H9.
+       destruct H9. destruct H9.
+       specialize (H8 x3 H9).
+       exists x3.
+       split; auto.
 Qed.
 
   
