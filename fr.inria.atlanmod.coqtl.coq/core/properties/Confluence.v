4e438a64e367c05f93f5642070061be7bda39194
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-10-09 19:41:30 +0200

    splitted properties in files

--- fr.inria.atlanmod.coqtl.coq/core/properties/Confluence.v
+++ fr.inria.atlanmod.coqtl.coq/core/properties/Confluence.v
@@ -13,80 +13,6 @@ Require Import FunctionalExtensionality.
 
 
 (*************************************************************)
-(** * Universality                                           *)
-(*************************************************************)
-
-Definition toTransformation (tc: TransformationConfiguration) (f: SourceModel -> TargetModel) := 
-  (buildTransformation 0 [
-    (buildRule "rule"%string 
-      (fun sm sp => match sp with nil => Some true | _ => Some false end)
-      (fun sm sp => Some (length (allModelElements (f sm))))
-      [(buildOutputPatternElement "out"%string 
-         (fun i sm sp => nth_error (allModelElements (f sm)) i)
-         (fun tls i sm sp te => match i with 0 => Some (allModelLinks (f sm)) | _ => None end))
-      ])
-  ]).
-
-Theorem universality :
-forall (tc: TransformationConfiguration) (f: SourceModel -> TargetModel),
-  (forall (sm: SourceModel), Model_wellFormed sm -> Model_wellFormed (f sm)) ->
-  exists (t: Transformation), 
-    forall (sm: SourceModel), Model_wellFormed sm -> execute t sm = f sm.
-Proof.
-  intros.
-  exists (toTransformation tc f).
-  intros.
-  unfold execute.
-  unfold applyPattern.
-  unfold applyRuleOnPattern.
-  unfold applyIterationOnPattern.
-  unfold applyElementOnPattern.
-  unfold evalOutputPatternLinkExpr.
-  unfold instantiatePattern.
-  unfold instantiateRuleOnPattern.
-  unfold instantiateIterationOnPattern.
-  unfold instantiateElementOnPattern.
-  unfold evalOutputPatternElementExpr.
-  unfold evalIteratorExpr.
-  unfold evalExpr.
-  simpl.
-  apply (H sm) in H0.
-  destruct (f sm). simpl.
-  f_equal.
-  - clear H. clear H0.
-    induction modelElements.
-    * reflexivity.
-    * simpl.
-      f_equal.
-      rewrite <- IHmodelElements at 2.
-      repeat rewrite flat_map_concat_map.
-      f_equal.
-      rewrite <- seq_shift.
-      rewrite map_map.
-      reflexivity.
-  - destruct modelElements eqn:dst.
-    * crush. 
-    * clear H0.
-      simpl. 
-      repeat rewrite app_nil_r.
-      rewrite app_nil_end.
-      f_equal.
-      apply in_flat_map_nil.
-      intros.
-      rewrite app_nil_r.
-      destruct a.
-      + exfalso. 
-        rewrite in_seq in H0.
-        lia.
-      + simpl.
-        rewrite in_seq in H0.
-        destruct H0.
-        simpl in H1.
-        apply Lt.lt_S_n in H1.
-        destruct (nth_error l a); reflexivity.
-Qed.
-
-(*************************************************************)
 (** * Confluence                                             *)
 (*************************************************************)
 
@@ -196,19 +122,3 @@ Proof.
                    admit. (* requires reordering for rules *)
                  }
 Admitted.
-
-(*************************************************************)
-(** * Monotonicity                                           *)
-(*************************************************************)
-
-Definition SourceModel_incl {tc: TransformationConfiguration}  (m1 m2: SourceModel) : Prop := 
-  incl (allModelElements m1) (allModelElements m2) /\
-  incl (allModelLinks m1) (allModelLinks m2).
-
-Definition TargetModel_incl {tc: TransformationConfiguration}  (m1 m2: TargetModel) : Prop := 
-  incl (allModelElements m1) (allModelElements m2) /\
-  incl (allModelLinks m1) (allModelLinks m2).
-
-Definition monotonicity (tc: TransformationConfiguration) (t: Transformation) :=
-  forall (sm1 sm2: SourceModel),
-  SourceModel_incl sm1 sm2 -> TargetModel_incl (execute t sm1) (execute t sm2).
