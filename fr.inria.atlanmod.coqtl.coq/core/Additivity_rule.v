0c2efe1d580c40e1926d4b3a5061836f7a36ac1b
Zheng Cheng <cz0590@gmail.com>
2021-10-09 18:38:12 +0200

    Update Additivity_rule.v

--- fr.inria.atlanmod.coqtl.coq/core/Additivity_rule.v
+++ fr.inria.atlanmod.coqtl.coq/core/Additivity_rule.v
@@ -11,15 +11,49 @@ Require Import PeanoNat.
 Require Import Lia.
 Require Import FunctionalExtensionality.
 
+
 (*************************************************************)
 (** * Additivity in Rule context                             *)
 (*************************************************************)
 
+Definition Transformation_incl_rules''' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
+  (Transformation_getArity t1 = Transformation_getArity t2) /\ 
+  subseq (Transformation_getRules t1) (Transformation_getRules t2). 
+
+
 Definition Transformation_incl_rules'' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
   (Transformation_getArity t1 = Transformation_getArity t2) /\ 
-  forall r: Rule, In r (Transformation_getRules t1) -> In r (Transformation_getRules t2). 
+  forall r: Rule, In r (Transformation_getRules t1) -> In r (Transformation_getRules t2).
 
-Theorem additivity_rules :
+
+Lemma tr_incl_equiv:
+  forall (tc: TransformationConfiguration) t1 t2,
+    Transformation_incl_rules''' t1 t2 -> Transformation_incl_rules'' t1 t2.
+Proof.
+intros.
+destruct  H.
+unfold Transformation_incl_rules''.
+split. 
+* auto.
+* intro.
+  induction H0.
+  + intros.
+    contradiction.
+  + intros.
+    simpl in H1.
+    simpl.
+    destruct H1.
+    - left. crush.
+    - right. crush.
+  + intros.
+    simpl.
+    right.
+    auto.
+Qed.
+
+
+
+Lemma additivity_rules_general :
 forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
   (Transformation_incl_rules'' t1 t2 -> 
     incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm))).
@@ -44,4 +78,15 @@ split.
   + apply filter_In.
     split; auto.
   + auto.
+Qed.
+
+Theorem additivity_rules:
+forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
+  (Transformation_incl_rules''' t1 t2 -> 
+    incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm))).
+Proof.
+intros.
+specialize (tr_incl_equiv tc t1 t2 H).
+specialize (additivity_rules_general tc t1 t2).
+auto.
 Qed.
