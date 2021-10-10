6b6047ceb896fc4443b054fe1e4d64ed6ca6df6f
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-10-10 14:13:32 +0200

    updating theorem statement

--- fr.inria.atlanmod.coqtl.coq/core/modeling/properties/ModelingAdditivityRule.v
+++ fr.inria.atlanmod.coqtl.coq/core/modeling/properties/ModelingAdditivityRule.v
@@ -136,6 +136,6 @@ Qed.
 
 Theorem arity_incl :
 forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
-  ((maxArity t1) <= (maxArity t2) /\ (Transformation_getRules t1) = (Transformation_getRules t2)) -> 
+  ((Transformation_getArity t1) <= (Transformation_getArity t2) /\ (Transformation_getRules t1) = (Transformation_getRules t2)) -> 
     incl (allModelElements (execute t1 sm)) (allModelElements (execute t2 sm)).
 Admitted.
