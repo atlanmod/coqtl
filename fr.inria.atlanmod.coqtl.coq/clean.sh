47a8173edd5e94319b22a67b31f3eabac1688c4d
Zheng Cheng <cz0590@gmail.com>
2021-10-25 11:46:45 +0200

    Update clean.sh

--- fr.inria.atlanmod.coqtl.coq/clean.sh
+++ fr.inria.atlanmod.coqtl.coq/clean.sh
@@ -3,3 +3,4 @@ find . -name "*.vo" -type f -delete
 find . -name "*.vos" -type f -delete
 find . -name "*.vok" -type f -delete
 find . -name "*.glob" -type f -delete
+find . -name "*.aux" -type f -delete
