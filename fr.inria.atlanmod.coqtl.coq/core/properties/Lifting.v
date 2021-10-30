e262aeb74c5ca57b497851fefdeee924b3e6fbbd
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2021-10-30 14:45:47 +0200

    sufficient version of Forall_rules

--- fr.inria.atlanmod.coqtl.coq/core/properties/Lifting.v
+++ fr.inria.atlanmod.coqtl.coq/core/properties/Lifting.v
@@ -70,6 +70,36 @@ Proof.
     crush.
 Qed.
 
+Theorem Forall_rules_suff :
+forall (tc: TransformationConfiguration) (p: TargetModelElement -> Prop) 
+  (sm: SourceModel) (tr: Transformation),
+    Forall 
+      (fun r => 
+        Forall 
+          (fun sp => Forall p (instantiateRuleOnPattern r sm sp)) 
+          (allTuples tr sm)) 
+      (Transformation_getRules tr)
+        -> Forall p (allModelElements (execute tr sm)).
+Proof.
+    simpl.
+    intros.
+    rewrite Forall_flat_map.
+    unfold instantiatePattern. 
+    unfold matchPattern.
+    rewrite !Forall_forall.
+     intros.
+      rewrite !Forall_forall.
+      intros.
+      apply in_flat_map in H1. repeat destruct H1.
+      apply filter_In in H1. destruct H1.
+      rewrite !Forall_forall in H.
+      specialize (H x1 H1).
+      rewrite !Forall_forall in H.
+      specialize (H x H0).
+      rewrite !Forall_forall in H.
+      crush.
+Qed.
+
 Theorem Forall_expressions :
 forall (tc: TransformationConfiguration) (p: TargetModelElement -> Prop) 
   (sm: SourceModel) (tr: Transformation),
@@ -82,8 +112,7 @@ forall (tc: TransformationConfiguration) (p: TargetModelElement -> Prop)
               (fun o => 
                 (evalGuardExpr r sm sp = Some true) ->
                   match evalOutputPatternElementExpr sm sp i o with
-                  | Some e => p e | None => True end
-              )
+                  | Some e => p e | None => True end)
               (Rule_getOutputPatternElements r))
             (seq 0 (evalIteratorExpr r sm sp)))
           (allTuples tr sm))
